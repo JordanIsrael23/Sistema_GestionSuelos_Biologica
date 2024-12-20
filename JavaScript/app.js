902fb1ad2e57c65dee7947cb8aeb9f9970c3a173
Jordan Arcos <jordanarcos0@gmail.com>
2024-12-20 14:15:36 -0500

    BDD

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,26 +1,10 @@
 
-const express = require('express');
 const conexion = require('./database');
 
-const app = express();
-const puerto = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Servidor funcionando correctamente');
-});
-
-
-app.get('./tipos_usuarios',(req,res) => {
-    conexion.query('SELECT * FROM tipos_usuarios',(err, results) => {
-        if (err){
-            console.error('Error en la consulta:', err);
-            res.status(500).send('error en el servidor');
-            return;
-        }
-        res.json(results);
-    });
-});
-
-app.listen(puerto,()=>{
-    console.log(`Servidor ejecutándose en http://localhost:${puerto}`);
+conexion.connect((err)=>{
+    if(err){
+        console.error('no se pudo conectar',err);
+    } else{
+        console.log('Conexion exitosa');
+    }
 });
