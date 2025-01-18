35c26e8a2d534ae7d517051d19fc0d8fa7af2180
Jordan Arcos <jordanarcos0@gmail.com>
2025-01-17 23:22:35 -0500

    Control de Datos en Registro

---
+++ JavaScript/formulario.js
@@ -0,0 +1,47 @@
+
+const cedula = document.getElementById('cedula');
+const telefono = document.getElementById('telefono');
+
+const nombres = document.getElementById('nombre');
+const apellidos = document.getElementById('apellido');
+
+const email = document.getElementById('email');
+
+const formulario = document.getElementById('registroForm');
+
+cedula.addEventListener('input',(e)=>{
+    e.target.value = e.target.value.replace(/[^0-9]/g,'');
+});
+
+
+telefono.addEventListener('input',(e)=>{
+    e.target.value = e.target.value.replace(/[^0-9]/g,'');
+});
+
+
+formulario.addEventListener('submit',(e)=>{
+    if(cedula.value.length !== 10){
+        e.preventDefault();
+        alert('El ID debe tener 10 digitos');
+    }
+    if(telefono.value.length !== 10){
+        e.preventDefault();
+        alert('El telefono debe tener 10 digitos');
+    }
+});
+
+nombres.addEventListener('input',(e)=>{
+    e.target.value = e.target.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g,'');
+});
+
+apellidos.addEventListener('input',(e)=>{
+    e.target.value = e.target.value.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g,'');
+});
+
+email.addEventListener('input',(e)=>{
+    e.target.value = e.target.value.replace(/[^a-zA-Z0-9@.\-_]/g,'');
+})
+
+
+
+
